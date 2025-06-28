1f6f38c128066e54cd6d351872459a6f5f884fa0
Mango-Potato <nytest8585@gmail.com>
2025-06-28 11:12:14 +0800

    Updated AiChef.tsx

--- src/pages/AiChef.tsx
+++ src/pages/AiChef.tsx
@@ -62,12 +62,8 @@ const AiChef = () => {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-6">
         <div className="lg:col-span-1 space-y-4">
           <KitchenInventory />
-          {/* <RecipeSuggestions onSelectRecipe={handleSelectRecipe} /> */}
-          <RecipeSuggestions
-            inventory={inventory}
-            onSelectRecipe={handleSelectRecipe}
-          />
-
+          <RecipeSuggestions onSelectRecipe={handleSelectRecipe} />
+          
         </div>
         
         <div className="lg:col-span-2" id="recipe-section">
